7dfcef3fb1c07309b38e4c244534443d2a3dd568
adaruiz <ada.ruiz93@gmail.com>
2021-09-30 09:54:17 -0700

    updated app state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
   const [bucketList, setBucketList] = useState("")
+
+  const getBucketList = async () => {
+    try{
+      const getList = await fetch("http://localhost:8080/blist")
+      const jsonList = await getList.json()
+      setBucketList(jsonList)
+      console.log(bucketList, jsonList)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getBucketList()
+  }, [])
+
   return (
     <div className="App">
       <h1>Bucket List</h1>
